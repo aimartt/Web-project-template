1fd1f14b6b32e3103e3778c6c3473e5c845c555e
aimartt <aimartt@gmail.com>
2015-03-31 12:13:08 +0800

    Update comments and gitignore file

--- src/main/java/com/aimartt/example/TestService.java
+++ src/main/java/com/aimartt/example/TestService.java
@@ -1,9 +1,5 @@
 /**
- * Copyright (c) 2009 FEINNO, Inc. All rights reserved.
- * This software is the confidential and proprietary information of 
- * FEINNO, Inc. You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the 
- * license agreement you entered into with FEINNO.
+ * 
  */
 package com.aimartt.example;
 
